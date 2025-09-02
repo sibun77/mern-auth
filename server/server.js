10716edb7f517ee7c998ee7c32c42b3ef4e86729
Shibabrata Jena <shibabrataj7@gmail.com>
2025-09-02 23:49:17 +0530

    trying to solve cors probleam for forth time

--- server/server.js
+++ server/server.js
@@ -9,23 +9,15 @@ import userRouter from './routes/userRoutes.js';
 const app = express();
 connectDB();
 
-const allowedOrigins = ['https://mern-auth-indol-beta.vercel.app', 'https://mern-auth-indol-beta.vercel.app/' , ]
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cors({ origin: 'https://mern-auth-indol-beta.vercel.app', credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
-
 
 //API Endpoints
 app.get('/', (req, res) => res.send("API working"))
 app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
 
-
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 5000
-    app.listen(PORT, () => console.log("Server is running on PORT: " + PORT))
-}
-
-//export server for vercel
+// Export the app instance for Vercel
 export default app;
