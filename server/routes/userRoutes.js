1199275ac33663bd2cb5029a13d1420d3491bbdb
Shibabrata Jena <shibabrataj7@gmail.com>
2025-09-03 13:32:18 +0530

    clean up the old code

--- server/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import userAuth from '../middleware/userAuth.js';
-import { getUserData } from '../controllers/userController.js';
-
-const userRouter = express.Router();
-userRouter.post('/data', userAuth, getUserData);
-export default userRouter;
