9d2c137145977acd1b0aba686c912debcff677f7
Shibabrata Jena <shibabrataj7@gmail.com>
2025-09-03 06:33:14 +0530

    debugging

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -7,6 +7,7 @@ const userAuth = async (req, res, next) => {
     }
     try {
         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
+        console.log(tokenDecode.id)
         if (tokenDecode.id) {
             req.body.userId = tokenDecode.id;
         } else {
