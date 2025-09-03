80ba5a0e242675b22d9291342e6821e025ec93d1
Shibabrata Jena <shibabrataj7@gmail.com>
2025-09-03 13:37:11 +0530

    rebuild the code

---
+++ server/routes/authRoute.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { isAuthenticatd, login, logout, register, resetPassword, sendResetOtp, sendVerifyOtp, verifyEmail } from '../controllers/authController.js';
+import userAuth from '../middleware/userAuth.js';
+const authRouter = express.Router();
+authRouter.post('/register', register);
+authRouter.post('/login', login);
+authRouter.post('/logout', logout);
+authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
+authRouter.post('/verify-account', userAuth, verifyEmail);
+authRouter.post('/is-auth', userAuth, isAuthenticatd);
+authRouter.post('/send-reset-otp', sendResetOtp);
+authRouter.post('/reset-password', resetPassword);
+
+
+export default authRouter;
